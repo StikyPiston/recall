f6c9219e733c5352999d1493e9a9eae8c77d9720
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-15 17:49:41 +0200

    feat: initial clap cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use clap::{Parser, Subcommand};
+
+static VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Parser)]
+#[command(name = "recall")]
+#[command(version = &VERSION)]
+#[command(about = "A minimal to-do list with a few amenities", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { name: String, priority: u16 },
+    Backburner { id: u32 },
+    Busy { id: u32 },
+    Clean,
+    Clear,
+    Done { id: u32 },
+    List,
+    Undo { id: u32 },
+    Xp,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Add { name, priority } => {
+            println!(
+                "called 'add' with name as {} and priority as {}",
+                name, priority
+            );
+        }
+        Commands::Backburner { id } => {
+            println!("called 'backburner' with id as {}", id);
+        }
+        Commands::Busy { id } => {
+            println!("called 'busy' with id as {}", id);
+        }
+        Commands::Clean => {
+            println!("called 'clean'");
+        }
+        Commands::Clear => {
+            println!("called 'clear'");
+        }
+        Commands::Done { id } => {
+            println!("called 'done' with id as {}", id);
+        }
+        Commands::List => {
+            println!("called 'list'");
+        }
+        Commands::Undo { id } => {
+            println!("called 'undo' with id of {}", id);
+        }
+        Commands::Xp => {
+            println!("called 'xp'");
+        }
+    }
 }
