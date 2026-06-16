3a310f42e6cd711f3b262afd9edfa89964dbcf04
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-16 11:32:37 +0200

    feat: ensure_file(), init_storage()

--- src/help.rs
+++ src/help.rs
@@ -1,6 +1,9 @@
 use dirs;
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::Path;
 
+// MARK: types
 #[derive(Serialize, Deserialize)]
 struct Task {
     name: String,
@@ -15,6 +18,7 @@ struct XP {
     last_checked: u64,
 }
 
+// MARK: directory helpers
 pub fn home_dir() -> String {
     let dir = dirs::home_dir();
     return dir.map(|p| p.to_string_lossy().into_owned()).unwrap();
@@ -26,4 +30,16 @@ pub fn todo_path() -> String {
 
 pub fn xp_path() -> String {
     home_dir() + "/.recall_xp"
+}
+
+// MARK: save/load helpers
+pub fn ensure_file(path: &str, content: &[u8]) {
+    if !Path::new(path).exists() {
+        let _ = fs::write(path, content);
+    }
+}
+
+pub fn init_storage() {
+    ensure_file(&todo_path(), b"[]");
+    ensure_file(&xp_path(), br#"{"XP":0,"last_checked":0}"#);
 }
